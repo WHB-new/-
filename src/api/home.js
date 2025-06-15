e2b8f432d891ea8f02973420ba81e7d43fa64d6b
WHB-new <350033026@qq.com>
2025-06-15 12:19:47 +0800

    添加api

---
+++ src/api/home.js
@@ -0,0 +1,7 @@
+//请求api工具
+import request from "@/utils/request";
+// 参考形式
+// export const 方法名 = ()=>{
+//   url:'/getList',
+//   method:'get',
+// }
---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { ElMessage } from 'element-plus';
+const request = axios.create({
+  baseURL: 'http://localhost:8000',
+  timeout:10000,
+
+})
+// 请求拦截器
+request.interceptors.request.use(config => {
+  //后期加上注册登录进行token验证
+  return config
+})
+// 响应拦截器
+request.interceptors.response.use(res => {
+   const status = res.data.code || 200
+   const message = res.data.msg || '未知错误'
+   if(status === 401){
+    //没权限，后期可跳转到注册登录页面
+    ElMessage.warning('没有权限，请先登录')
+    return Promise.reject(new Error(message))
+   }
+   if(status != 200){
+    ElMessage.warning('错误码' + status + "" + message)
+    return Promise.reject(new Error(message))
+   }
+},err=>{
+  // 错误处理
+  ElMessage.warning('接口请求失败')
+  return Promise.reject(new Error(err))
+})
+export default request
